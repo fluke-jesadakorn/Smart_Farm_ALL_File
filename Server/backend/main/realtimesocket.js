25e09f3b672c7897405b75ffd853ba05b75851a7
fluke-jesadakorn <jesadakorn.kirtnu@gmail.com>
2019-09-30 00:23:25 +0700

    beta graph nbserv

--- Server/backend/main/realtimesocket.js
+++ Server/backend/main/realtimesocket.js
@@ -1,36 +1,39 @@
 module.exports = {server} 
 function server(){
-var express = require('express') 
-var bodyParser =require('body-parser') 
-var http = require('http') 
-var socketIO =require ('socket.io')
+    var express = require('express') 
+    var bodyParser =require('body-parser') 
+    var http = require('http') 
+    var socketIO =require ('socket.io')
+    var store = require('./store')
 
-const server = express()
-const port = 3003;
+    const server = express()
+    const port = 3003;
 
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({
-    extended: true
-}))
+    server.use(bodyParser.json())
+    server.use(bodyParser.urlencoded({
+        extended: true
+    }))
 
-const app = server.listen(port, function (err, result) {
-    console.log('running in port http://localhost:' + port)
-})
-
-const io = socketIO.listen(app);
-// รอการ connect จาก client
-io.on('connection', client => {
-    console.log('user connected')
-  
-    // เมื่อ Client ตัดการเชื่อมต่อ
-    client.on('disconnect', () => {
-        console.log('user disconnected')
+    const app = server.listen(port, function (err, result) {
+        console.log('running in port http://localhost:' + port)
     })
 
-    // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
-    client.on('sent-message', function (message) {
-        io.sockets.emit('new-message', message)
-    })
-})
+    const io = socketIO.listen(app);
+    // รอการ connect จาก client
+    io.on('connection', client => {
+        console.log('user connected')
+    
+        // เมื่อ Client ตัดการเชื่อมต่อ
+        client.on('disconnect', () => {
+            console.log('user disconnected')
+        })
 
+        // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
+        client.on('sent-message', function (message) {
+            //ส่งข้อความที่รอรับจาก client
+            io.sockets.emit('new-message', message)
+            //เอา Temp กระจายทุก client
+            io.sockets.emit('new-message', store.temp)
+        })
+    })
 }
--- Server/frontend/src/Component/graph.js
+++ Server/frontend/src/Component/graph.js
@@ -1,107 +1,3 @@
-/*import React, { Component } from 'react';
-import CanvasJSReact from './assets/canvasjs.react';
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
-var dataPoints1 = [];
-var dataPoints2 = [];
-var updateInterval = 2000;
-//initial values
-var yValue1 = 408;
-var yValue2 = 350;
-var xValue = 5;
-export default class Graph extends Component {
-	constructor() {
-		super();
-		this.updateChart = this.updateChart.bind(this);		
-	}
-	componentDidMount(){
-		this.updateChart(20);
-		setInterval(this.updateChart, updateInterval);
-	}
-	
-	updateChart(count) {
-		count = count || 1;		
-		for (var i = 0; i < count; i++) {
-			xValue += 2;
-			yValue1 = Math.floor(Math.random()*(408-400+1)+400);
-			yValue2 = Math.floor(Math.random()*(350-340+1)+340);
-			dataPoints1.push({
-			  x: xValue,
-			  y: yValue1
-			});
-			dataPoints2.push({
-			  x: xValue,
-			  y: yValue2
-			});
-		}
-		this.chart.options.data[0].legendText = " Bugatti Veyron - " + yValue1 + " km/h";
-		this.chart.options.data[1].legendText = " Lamborghini Aventador - " + yValue2 + " km/h";
-		this.chart.render();
-	}
-	render() {
-		const options = {
-			zoomEnabled: true,
-			theme: "light2",
-			title: {
-				text: "Speed of Bugatti vs Lamborghini"
-			},
-			axisX: {
-				title: "chart updates every 2 secs"
-			},
-			axisY:{
-				suffix: " km/h",
-				includeZero: false
-			},
-			toolTip: {
-				shared: true
-			},
-			legend: {
-				cursor:"pointer",
-				verticalAlign: "top",
-				fontSize: 18,
-				fontColor: "dimGrey",
-				itemclick : function(e){
-					if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-						e.dataSeries.visible = false;
-					}
-					else {
-						e.dataSeries.visible = true;
-					}
-					e.chart.render();
-				}
-			},
-			data: [
-				{
-					type: "stepLine",
-					xValueFormatString: "#,##0 seconds",
-					yValueFormatString: "#,##0 km/h",
-					showInLegend: true,
-					name: "Bugatti Veyron",
-					dataPoints: dataPoints1
-				},
-				{
-					type: "stepLine",
-					xValueFormatString: "#,##0 seconds",
-					yValueFormatString: "#,##0 km/h",
-					showInLegend: true,
-					name: "Lamborghini Aventador" ,
-					dataPoints: dataPoints2
-				}
-			]
-		}
-		
-		return (
-		<div>
-			<h1>React Dynamic Multi Series Chart</h1>
-			<CanvasJSChart options = {options} 
-				onRef={ref => this.chart = ref}
-			/>
-			{}
-		</div>
-		);
-	}
-}*/
-
 import React, { Component } from 'react'
 import socketIOClient from 'socket.io-client'
 import Chart from "react-apexcharts";
@@ -110,24 +6,27 @@ class Graph extends Component {
   constructor() {
     super()
     this.state = {
-      input: '',
-      message: [],
-	  endpoint: "http://localhost:3003", // เชื่อมต่อไปยัง url ของ realtime server
-	  
-	  options: {
-        chart: {
-          id: "basic-bar"
-        },
-        xaxis: {
-          categories: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-        }
-      },
-      series: [
-        {
-          name: "Temp",
-          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        }
-      ]
+		//for SocketIO
+      	input: '',
+		message: [],
+		endpoint: "http://localhost:3003", // เชื่อมต่อไปยัง url ของ realtime server
+		
+		//for ApexChart
+		options: {
+			chart: {
+				id: "basic-bar"
+			},
+			xaxis: {
+				categories: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+			}
+		},
+		series: [
+			{
+				name: "Temp",
+				data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+			}
+		],
+
     }
   }
 
@@ -139,12 +38,15 @@ class Graph extends Component {
   send = (message) => {
     const { endpoint, input } = this.state
     const socket = socketIOClient(endpoint)
-    socket.emit('sent-message', input)
-
-	//pop เมื่อ Data มา
+	socket.emit('sent-message', input)
+	
+	//เอาไว้ใส่ Debug
+	this.setState({input:''})
+	//push เมื่อ Data มา จาก index สุดท้าย
 	this.state.series[0].data.push(input);
 	//shift Array ทิ้งใน index[0] ไล่ซ้าย
 	this.state.series[0].data.shift()
+	console.log("Pushed and Poped data")
   }
 
   // รอรับข้อมูลเมื่อ server มีการ update
@@ -166,24 +68,24 @@ class Graph extends Component {
 	const { input, message } = this.state  
 	  
     return (
-      <div>
+      <div style={style}>
+		<Chart
+			options={this.state.options}
+			series={this.state.series}
+			type="line"
+			width="500"
+        />
         <div style={style}>
-          <input value={input} onChange={this.changeInput} />
-          <button onClick={() => this.send()}>Send</button>
+			<input value={input} onChange={this.changeInput} />
+			<button onClick={() => this.send()}>Send</button>
         </div>
         {
           message.map((data, i) =>
             <div key={i} style={style} >
-              {i + 1} : {data}
+				{i + 1} : {data}
             </div>
           )
 		}
-		<Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="line"
-              width="500"
-        />
       </div>
     )
   }
