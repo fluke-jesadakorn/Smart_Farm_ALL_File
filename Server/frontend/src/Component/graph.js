63063c6f2105e00872c301c485c32ee691e2ccfc
fluke-jesadakorn <jesadakorn.kirtnu@gmail.com>
2019-10-16 00:24:37 +0700

    real time graph can dynamic

--- Server/frontend/src/Component/graph.js
+++ Server/frontend/src/Component/graph.js
@@ -34,7 +34,7 @@ export default class App extends Component {
       series: [
         {
           name: "temp",
-          data: [1,2,3,4,5,6,7,8,9,10]
+          data: []
         }
       ]
     };
